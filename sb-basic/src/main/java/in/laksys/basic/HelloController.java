2df7895fd85ce9987a71720e732d00d3379114ec
laksys <laksys@RBPC.laksys.in>
2025-11-04 14:43:00 +0530

    initial commit

---
+++ sb-basic/src/main/java/in/laksys/basic/HelloController.java
@@ -0,0 +1,13 @@
+package in.laksys.basic;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping
+	public ResponseEntity<String> sayHello(){
+		return ResponseEntity.ok("Hello, World!");
+	}
+}
